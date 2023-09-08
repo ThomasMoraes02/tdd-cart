9d15d00cbf82584f44ca84f8f84dd58c2c5ede56
Thomas Moraes <tho.vini7@gmail.com>
2023-09-08 13:26:22 -0300

    Criando Produto

--- create-product-sqlite.php
+++ create-product-sqlite.php
@@ -1,7 +1,7 @@
 <?php
 
 use Cart\Model\Product;
-use Cart\Persistance\ProductRepositoryMysql;
+use Cart\Infra\Persistance\ProductRepositoryMysql;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
