02e438e93517e549cbc3760476750f1168d871e7
Thomas Moraes <tho.vini7@gmail.com>
2023-09-07 23:05:35 -0300

    Persistindo Produtos em SQlite

---
+++ create-product-sqlite.php
@@ -0,0 +1,23 @@
+<?php
+
+use Cart\Model\Product;
+use Cart\Persistance\ProductRepositoryMysql;
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+$pdo = new PDO('sqlite:database/db.sqlite');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->exec("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, price FLOAT, quantity INTEGER)");
+
+$product = new Product('Notebook Dell G15', 5200, 5);
+
+$pdo->beginTransaction();
+
+$productRepository = new ProductRepositoryMysql($pdo);
+$productRepository->save($product);
+
+$product = $productRepository->findById(1);
+
+$pdo->commit();
+
+var_dump($product);
---
+++ src/Persistance/ProductRepositoryMysql.php
@@ -0,0 +1,72 @@
+<?php 
+namespace Cart\Persistance;
+
+use PDO;
+use Cart\Model\Product;
+use Cart\Model\Repository\ProductRepository;
+
+class ProductRepositoryMysql implements ProductRepository
+{
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function save(Product $product): Product
+    {
+        $sql = 'INSERT INTO products (name, price, quantity) VALUES (:name, :price, :quantity)';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue('name', $product->getName());
+        $stmt->bindValue('price', $product->getPrice());
+        $stmt->bindValue('quantity', $product->getQuantity());
+        $stmt->execute();
+
+        return new Product($product->getName(), $product->getPrice(), $product->getQuantity(), $this->pdo->lastInsertId());
+    }
+
+    public function findById(int $id): ?Product
+    {
+        $sql = "SELECT * FROM products WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue('id', $id);
+        $stmt->execute();
+
+        $product = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $product ? new Product($product['name'], $product['price'], $product['quantity'], $product['id']) : null;
+    }
+
+    public function update(int $id, Product $product): ?Product
+    {
+        $sql = "UPDATE products SET name = :name, price = :price, quantity = :quantity WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue('name', $product->getName());
+        $stmt->bindValue('price', $product->getPrice());
+        $stmt->bindValue('quantity', $product->getQuantity());
+        $stmt->bindValue('id', $id);
+
+        $stmt->execute();
+
+        return $this->findById($id);
+    }
+
+    public function delete(int $id): bool
+    {
+        $sql = "DELETE FROM products WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue('id', $id);
+
+        return $stmt->execute();
+    }
+
+    public function getAll(): array
+    {
+        $sql = "SELECT * FROM products";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
